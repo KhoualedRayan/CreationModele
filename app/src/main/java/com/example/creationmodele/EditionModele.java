34ee83120197bf13b4c388a86b7f8292fd7d864b
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2023-11-17 14:30:15 +0100

    Sauvegarde Modele en format json dans les fichiers

--- app/src/main/java/com/example/creationmodele/EditionModele.java
+++ app/src/main/java/com/example/creationmodele/EditionModele.java
@@ -2,7 +2,11 @@ package com.example.creationmodele;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import Habitation.Modele;
 
 public class EditionModele extends AppCompatActivity {
 
@@ -10,5 +14,7 @@ public class EditionModele extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edition_modele);
+        @SuppressLint({"MissingInflatedId", "LocalSuppress"}) TextView textView = findViewById(R.id.textView_nom_Modele);
+        textView.setText(textView.getText()+Modele.getInstance().getNom());
     }
 }
--- app/src/main/java/com/example/creationmodele/MainActivity.java
+++ app/src/main/java/com/example/creationmodele/MainActivity.java
@@ -2,26 +2,36 @@ package com.example.creationmodele;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
 
 import Habitation.Modele;
 
 public class MainActivity extends AppCompatActivity {
-    public Modele modeleActuel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button creerModele = findViewById(R.id.button_creation_modele);
         Button chargerModele = findViewById(R.id.button_charger_modele);
+        Button editerModele = findViewById(R.id.button_edition_modele);
 
         sendIntent(creerModele,CreationModele.class);
         sendIntent(chargerModele,ChargementModele.class);
+        sendIntent(editerModele, EditionModele.class);
 
     }
     public void sendIntent(Button button, Class c){
@@ -33,4 +43,34 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void SauvegarderModele(View view) {
+        try {
+            Context context = this;
+            // Créer un objet ObjectMapper
+            ObjectMapper objectMapper = new ObjectMapper();
+
+            // Convertir l'objet en format JSON
+            String json = objectMapper.writeValueAsString(Modele.getInstance());
+
+            // Spécifier le nom du fichier
+            String nomFichier = Modele.getInstance().getNom()+".json";
+
+            // Obtenir le répertoire de stockage interne de l'application
+            File repertoireInterne = context.getFilesDir();
+
+            // Créer un objet File pour le fichier de destination
+            File fichierDestination = new File(repertoireInterne, nomFichier);
+
+            // Écrire le JSON dans le fichier de destination
+            try (FileOutputStream fos = new FileOutputStream(fichierDestination)) {
+                fos.write(json.getBytes());
+            }
+            Log.i("MainACTIVITY : ","Sauvegarde en JSON réussie dans : " + fichierDestination.getAbsolutePath());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
