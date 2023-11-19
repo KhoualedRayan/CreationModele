9793f97b95a947c9523f46dbb4d5b214063fda7f
khoualed2u <rayan.khoualed3@etu.univ-lorraine.fr>
2023-11-19 20:01:05 +0100

    Ajout de la boussole pour la photo

--- app/src/main/java/Outils/AccelVectorView.java
+++ app/src/main/java/Outils/AccelVectorView.java
@@ -1,5 +1,69 @@
 package Outils;
 
-public class AccelVectorView {
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class AccelVectorView extends View {
+    private Paint paint;
+    private Paint paint2;
+    private float[] orientation,accel;
+    public AccelVectorView(Context context) {
+        super(context);
+        init();
+    }
+    public AccelVectorView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    private void init() {
+        this.orientation = new float[3];
+        this.accel = new float[3];
+        paint = new Paint();
+        paint2 = new Paint();
+        paint.setStrokeWidth(10);
+        paint.setColor(Color.GREEN);
+        paint2.setStrokeWidth(10);
+        paint2.setColor(Color.BLUE);
+    }
+    public void setAccelXY(float[] accel){
+        this.accel = accel;
+        invalidate();
+    }
+
+    public void setOrientation(float[] orientation){
+        this.orientation = orientation;
+        invalidate();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        //boussole
+        float x = (float) -Math.sin(orientation[0]);
+        float y = (float) -Math.cos(orientation[0]);
+        float centreX = (getWidth() / 2) ;
+        float centreY = (float) (getHeight() / 2.5);
+
+        canvas.drawLine(centreX, centreY, (x*80+centreX), (y*80+centreY), paint2);
+        //accelerometre
+        float startX = this.getWidth() / 2;
+        float startY = this.getHeight() - getHeight() /4;
+        float stopX = startX - accel[0] * 50;
+        float stopY = startY + accel[1] *50;
+
+        //canvas.drawLine(startX, startY, stopX, stopY, paint);
+
+
+
+
+
+
+    }
 
 }
--- app/src/main/java/com/example/creationmodele/PieceActivity.java
+++ app/src/main/java/com/example/creationmodele/PieceActivity.java
@@ -2,15 +2,47 @@ package com.example.creationmodele;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
+import android.widget.Switch;
 
-public class PieceActivity extends AppCompatActivity {
+import Outils.AccelVectorView;
 
+public class PieceActivity extends AppCompatActivity implements SensorEventListener {
+    private Switch capteur;
+    private SensorManager sensorManager;
+    private Sensor accelerometre;
+    private Sensor magnetometre;
+    private boolean isSensorEnabled = false;
+    private AccelVectorView accelVectorView;
+    private static final int FPS = 1000 / 30; // 30 fps
+    private final Handler handler = new Handler();
+    private float[] matriceR;
+    private float[] accel;
+    private float[] magneto;
+    private float[] orientation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_piece);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        accelerometre = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometre = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        capteur = findViewById(R.id.capteur_switch);
+        accelVectorView = findViewById(R.id.accelVectorView);
+        matriceR = new float[9];
+        accel = new float[3];
+        magneto = new float[3];
+        orientation = new float[3];
     }
 
     public void ajouterImageOuest(View view) {
@@ -26,5 +58,87 @@ public class PieceActivity extends AppCompatActivity {
     }
 
     public void Valider(View view) {
+    }
+
+    public void isTelMoving(float accelx, float accely, float accelz){
+        double omegaMagnitude = Math.sqrt(accelx * accelx + accely * accely +accelz * accelz );
+        double gravityMagnitude = SensorManager.STANDARD_GRAVITY;
+        double tolerance = 0.2;
+        if(Math.abs(omegaMagnitude - gravityMagnitude) < tolerance){
+            Log.i("Capteur : ","IMMOBILE");
+        }else {
+            Log.i("Capteur : ","MOBILE");
+        }
+    }
+    public void switchOnClick(View view){
+        if(isSensorEnabled){
+            sensorManager.unregisterListener(this);
+            capteur.setText("Capteur Désactivé");
+            isSensorEnabled = false;
+        }else{
+            sensorManager.registerListener(this, accelerometre, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, magnetometre, SensorManager.SENSOR_DELAY_NORMAL);
+            capteur.setText("Capteur Activé");
+            isSensorEnabled = true;
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isSensorEnabled) {
+            sensorManager.registerListener(this, accelerometre, SensorManager.SENSOR_DELAY_NORMAL);
+            sensorManager.registerListener(this, magnetometre, SensorManager.SENSOR_DELAY_NORMAL);
+            handler.postDelayed(updateViewRunnable, FPS);
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isSensorEnabled) {
+            sensorManager.unregisterListener(this);
+            handler.removeCallbacks(updateViewRunnable);
+
+        }
+    }
+
+    private final Runnable updateViewRunnable = new Runnable() {
+        @Override
+        public void run() {
+            // Rafraichissement de la vue selon les fps
+            accelVectorView.invalidate();
+            handler.postDelayed(this, FPS);
+        }
+    };
+
+    public void capteurOnClick(View view) {
+        switchOnClick(view);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(event.sensor == accelerometre) {
+            Log.i("Capteur", event.sensor.toString());
+            float accelx = event.values[0];
+            float accely = event.values[1];
+            float accelz = event.values[2];
+            accel = event.values;
+            accelVectorView.setAccelXY(accel);
+            isTelMoving(accelx, accely, accelz);
+        }
+        if (event.sensor == magnetometre) {
+            magneto = event.values;
+            SensorManager.getRotationMatrix(matriceR,null,accel,magneto);
+            SensorManager.getOrientation(matriceR,orientation);
+            accelVectorView.setOrientation(orientation);
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
